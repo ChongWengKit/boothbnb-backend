f8228472f962944f6231936dddb3e62bc8c6c9e9
chong wengkit <wengkitchong87@gmail.com>
2026-04-23 20:09:46 +0800

    i just dun know

--- dist/index.js
+++ dist/index.js
@@ -35,7 +35,7 @@ app.use(cors({
     },
     credentials: true
 }));
-app.options(/^(.*)$/, cors());
+app.options('*splat', cors());
 app.use('/webhook', webhookRoutes);
 app.use(express.json());
 app.use('/auth', authRoutes);
