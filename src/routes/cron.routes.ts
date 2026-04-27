d2396584d631b275f3140c2d759f4bdc7b87edd1
chong wengkit <wengkitchong87@gmail.com>
2026-04-27 19:09:38 +0800

    cron job for curreny update

--- src/routes/cron.routes.ts
+++ src/routes/cron.routes.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import { bookingCleanupHandler, emailRetryHandler, emailSyncHandler } from '../controllers/cron.controller.js';
+import { bookingCleanupHandler, currencyUpdateHandler, emailRetryHandler, emailSyncHandler } from '../controllers/cron.controller.js';
 import { authenticateCron } from '../middleware/cron.js';
 
 const router = Router();
@@ -9,5 +9,5 @@ router.use(authenticateCron);
 router.get('/booking-cleanup', bookingCleanupHandler);
 router.get('/email-retry', emailRetryHandler);
 router.get('/email-sync', emailSyncHandler);
-
+router.get('/currency-update', currencyUpdateHandler)
 export default router;
