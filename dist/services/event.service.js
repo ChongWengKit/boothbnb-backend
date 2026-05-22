b07b41eb7bdc0b5459298a469463e2bcb4bb445e
chong wengkit <wengkitchong87@gmail.com>
2026-05-22 15:53:28 +0800

    test

--- dist/services/event.service.js
+++ dist/services/event.service.js
@@ -469,7 +469,7 @@ export const getEventDetailsBySlug = async (slug) => {
             bookingSummaries.push({
                 vendor: booking.vendor,
                 booth_name: booth.name,
-                amount: booking.amount,
+                amount: Number(booking.amount),
                 currency_code: booking.currency_code,
                 status: isPaid ? 'PAID' : 'RESERVED',
                 booked_at: booking.booked_at
